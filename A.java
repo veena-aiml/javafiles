318e0cc26d0efaf01eef785145933c1c176511b5
veena-aiml <gudeveena@gmail.com>
2025-07-01 19:17:31 +0530

    javafiles-a

---
+++ A.java
@@ -0,0 +1,16 @@
+interface print
+{
+void message();
+}
+class A implements print
+{
+public void message()
+{
+System.out.println("hai");
+}
+public static void main(String args[])
+{
+A a=new A();
+a.message();
+}
+}
